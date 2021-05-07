3d8ab2614fd9fd3d4b032d6ac8044d4c3ecb6718
Michael White <mwhite7372@gmail.com>
2021-05-07 16:12:43 -0500

    inital modal added for payment verification

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Pay a Customer (RTP)</h1>
+
+    <!-- <ul>
+      <li v-for="error in errors">{{ error }}</li>
+    </ul> -->
+
+    <div>
+      <h2>Payment</h2>
+      Payee Name: <input type="text" v-model="PayeeName" /><br />
+      Payee Account Number:
+      <input type="text" v-model="PayeeAccountNumber" /><br />
+      Confirm Payee Account Number:
+      <input type="text" v-model="ConfirmPayeeAccountNumber" /><br />
+      Select Payee Account type:
+      <input type="text" v-model="PayeeAccountType" /><br />
+      Select from Payee Account Type:
+      <input type="text" v-model="FromPayeeAccountType" /><br />
+      Amount: <input type="text" v-model="Amount" /><br />
+      Memo: <input type="text" v-model="Memo" /><br />
+
+      <button v-on:click="cancel">Cancel</button>
+      <button v-on:click="showPayment">Verify Payment</button>
+    </div>
+
+    <dialog id="payment-details">
+      <form method="dialog">
+        <h2>Payment Info</h2>
+        <p>PayeeName: {{ PayeeName }}</p>
+
+        <button v-on:click="updatePlace(currentPlace)">Make Payment</button>
+        <button>Cancel</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
+<style></style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      // message: "Welcome to Vue.js!",
+      errors: [],
+      PayeeName: "",
+      ConfirmPayeeAccountNumber: "",
+      PayeeAccountType: "",
+      FromPayeeAccountType: "",
+      Amount: "",
+      Memo: "",
+      currentPlace: {},
+    };
+  },
+  created: function () {},
+  methods: {
+    showPayment: function () {
+      console.log(this.PayeeName);
+      document.querySelector("#payment-details").showModal();
+    },
+
+    createPayment: function () {
+      var params = {
+        name: this.PayeeName,
+        address: this.Amount,
+      };
+      console.log(params);
+    },
   },
 };
 </script>
