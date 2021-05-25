7297e0974a61aeccd2c4e80bd0be22f74eeeab83
Michael White <mwhite7372@gmail.com>
2021-05-25 12:14:09 -0500

    Added Vuetify to RTP-app

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
+import vuetify from "./plugins/vuetify";
 
 axios.defaults.baseURL = "http://localhost:8000";
 
@@ -9,5 +10,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
--- src/server.js
+++ src/server.js
@@ -1,112 +1,95 @@
-const path = require('path');
-const express = require('express');
-const Authenticator = require('./authenticator.js') 
-const FFDC = require('./ffdc.js');
+const path = require("path");
+const express = require("express");
+const Authenticator = require("./authenticator.js");
+const FFDC = require("./ffdc.js");
 const cors = require("cors");
 
 const app = express();
 const B2B = new Authenticator();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(
-    path.resolve(__dirname, '../dist'),
-    { maxAge: '1y', etag: false})
+app.use(
+  express.static(path.resolve(__dirname, "../dist"), {
+    maxAge: "1y",
+    etag: false,
+  })
 );
 app.use(cors());
 
-app.get('/api/login', async (req, res) => {
-    try {
-        var token = await B2B.getToken();
-        res.setHeader('Content-Type', 'application/json');
-        res.json(token);
-    } catch(err) {
-        res.status(500).send(err);
-    };   
-})
+app.get("/api/login", async (req, res) => {
+  try {
+    var token = await B2B.getToken();
+    res.setHeader("Content-Type", "application/json");
+    res.json(token);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
 
-app.post('/api/payment', async (req, res) => {
-    console.log("in payment");
-    var data = 
-      {
-        "sourceId": "Fake Web Payment - RTP App",
-        "initiatingParty": "LOCALOFFICEUS1",
-        // MEMO field 
-        "paymentInformationId": req.body.paymentInformationId,
-        "requestedExecutionDate": "2021-05-04",
-        "instructedAmount": 
-          {
-            // USER SETTABLE AMOUNT FIELD
-            "amount": req.body.amount,
-            // CURENCY FIELD
-            "currency": req.body.currency
-              
-          },
-        "paymentIdentification": 
-          {
-              
-            "endToEndId": "0020012021001"
-              
-          },
-        "debtor": 
-          {
-          //   DEBTOR NAME field in app
-            "name": "First American"
-              
-          },
-        "debtorAgent": 
-          {
-          // Hardcoded
-            "identification": "020010001"
-              
-          },
-        "debtorAccountId": 
-          {
-          // hardcoded to identify First American in RTP App
-            "identification": "276395636"
-              
-          },
-        "creditor": 
-          {
-          //   CREDITOR NAME FIELD IN APP
-            "name": req.body.creditor
-              
-          },
-        "creditorAgent": 
-          {
-          // 131000000 - Bank of America *or* 000000007 - CitiBank
-            "identification": "131000000"
-              
-          },
-        "creditorAccountId": 
-          
-          {
-          // HARDCODED FOR RTP APP
-            "identification": "1111111111"
-          },
-        "remittanceInformationUnstructured": "RmtInf7890"
-          
-      };
-    const url = "https://api.fusionfabric.cloud/payment/payment-initiation/realtime-payments/v2/us-real-time-payment/tch-rtps/initiate";
-  
-    try {
-      if (!req.body.token) {
-        res.status(500).send("Missing token!");
-      }
-          
-      const ffdc = new FFDC(req.body.token);
-      const result = await ffdc.callAPI(url, data);
-  
-      res.status(200).send(result);
-    } catch (err) {
-      res.status(500).send(err);
+app.post("/api/payment", async (req, res) => {
+  console.log("in payment");
+  var data = {
+    sourceId: "Fake Web Payment - RTP App",
+    initiatingParty: "LOCALOFFICEUS1",
+    // MEMO field
+    paymentInformationId: req.body.paymentInformationId,
+    requestedExecutionDate: "2021-05-04",
+    instructedAmount: {
+      // USER SETTABLE AMOUNT FIELD
+      amount: req.body.amount,
+      // CURENCY FIELD
+      currency: req.body.currency,
+    },
+    paymentIdentification: {
+      endToEndId: "0020012021001",
+    },
+    debtor: {
+      //   DEBTOR NAME field in app
+      name: "First American",
+    },
+    debtorAgent: {
+      // Hardcoded
+      identification: "020010001",
+    },
+    debtorAccountId: {
+      // hardcoded to identify First American in RTP App
+      identification: "276395636",
+    },
+    creditor: {
+      //   CREDITOR NAME FIELD IN APP
+      name: req.body.creditor,
+    },
+    creditorAgent: {
+      // 131000000 - Bank of America *or* 000000007 - CitiBank
+      identification: "131000000",
+    },
+    creditorAccountId: {
+      // HARDCODED FOR RTP APP
+      identification: "1111111111",
+    },
+    remittanceInformationUnstructured: "RmtInf7890",
+  };
+  const url =
+    "https://api.fusionfabric.cloud/payment/payment-initiation/realtime-payments/v2/us-real-time-payment/tch-rtps/initiate";
+
+  try {
+    if (!req.body.token) {
+      res.status(500).send("Missing token!");
     }
-      
-  });
+
+    const ffdc = new FFDC(req.body.token);
+    const result = await ffdc.callAPI(url, data);
+
+    res.status(200).send(result);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 app.listen(process.env.BACK_PORT || 8000, () => {
-    console.log(`Server is listening on port ${process.env.BACK_PORT}`);
+  console.log(`Server is listening on port ${process.env.BACK_PORT}`);
 });
